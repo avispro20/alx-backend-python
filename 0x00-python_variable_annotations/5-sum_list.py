b486aa40000493987f931fa3fc4e20ab3d275c5c
avispro20 <solomonok07@gmail.com>
2023-07-07 03:55:57 -0700

    Python variable annotation

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-"""Write a type-annotated funtion wum_list which takes a list
-input_list of floats as argument and returns their sun as a float.
+"""Write a type-annotated function sum_list which takes a list
+input_list of floats as argument and returns their sum as a float.
 """
 
 
-from typing import list
+from typing import List
 
 
-def sun_list(input_list: List[float]) -> flaot:
+def sum_list(input_list: List[float]) -> float:
     '''returns their sum as a float
     '''
     return float(sum(input_list))
